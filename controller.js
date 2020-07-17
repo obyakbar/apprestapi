fe2882ac792696cc3432025b480394df06f8eb3e
obyakbar <obyakbar@outlook.com>
2020-07-18 01:17:49 +0700

    mengubah file controller dan res

--- controller.js
+++ controller.js
@@ -4,5 +4,5 @@ var response = require('./res');
 var connection = require('./koneksi');
 
 exports.index = function(req, res) {
-    response.oke("Aplikasi res api berjalan")
+    response.ok("Aplikasi res api berjalan", res)
 }
